414a0d090b2c2aa1f7e6c9f5f6df741bc77fe4cf
Kyeongho Park <khpark@ncsoft.com>
2017-02-03 21:39:52 +0900

    LC-1628 #time 8h get 'window' property from 'window_capture' source.

--- src/obs.cc
+++ src/obs.cc
@@ -22,6 +22,16 @@ void Obs::ShutDown() {
 
 std::vector<std::string> Obs::FindAllWindowsOnDesktop() {
   std::vector<std::string> titles;
+
+  obs_source_t *source =
+      obs_source_create("window_capture", "Window Capture", nullptr, nullptr);
+  obs_properties_t *props = obs_source_properties(source);
+  obs_property_t *prop = obs_properties_get(props, "window");
+
+  // TODO(khpark): fill titles from prop, here.
+
+  obs_properties_destroy(props);
+  obs_source_release(source);
   return titles;
 }
 }  // namespace ncstreamer
