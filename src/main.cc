353a9d0824f9ce8cd52d58c435c2613e038a2ddc
Namki Kim <dkd1111@ncsoft.com>
2017-02-20 14:57:09 +0900

    LC-1750 #time 1h enable cef cache mode

--- src/main.cc
+++ src/main.cc
@@ -23,6 +23,17 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 
   CefSettings settings;
   settings.no_sandbox = true;
+  std::string temp_path = []() {
+    uint32 len = ::GetTempPath(0, NULL);
+    char *buf = new char[len];
+    ::GetTempPath(len, buf);
+    std::string path(buf);
+    delete buf;
+    buf = NULL;
+    return path;
+  }();
+  temp_path += "cef_cache";
+  CefString(&settings.cache_path) = temp_path.c_str();
 
   CefRefPtr<ncstreamer::App> app{new ncstreamer::App{hInstance}};
 
