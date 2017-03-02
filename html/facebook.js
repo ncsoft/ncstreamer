4772be5bd3db1ae5648639c72cbc130fab4c8097
Kyeongho Park <khpark@ncsoft.com>
2017-03-02 15:39:13 +0900

    minor indent

--- html/facebook.js
+++ html/facebook.js
@@ -10,72 +10,72 @@ importScript('//connect.facebook.net/en_US/sdk.js');
 
 
 const facebook = (function() {
-const exports = {
-  createLiveVideo: createFacebookLiveVideo,
-};
+  const exports = {
+    createLiveVideo: createFacebookLiveVideo,
+  };
 
 
-const facebookAppIds = {
-  production: /*NC Streaming*/ '1789696898019802',
-  alpha: /*NC Streaming - Alpha*/ '1795379417451550',
-};
-const facebookAppId = facebookAppIds.alpha;
+  const facebookAppIds = {
+    production: /*NC Streaming*/ '1789696898019802',
+    alpha: /*NC Streaming - Alpha*/ '1795379417451550',
+  };
+  const facebookAppId = facebookAppIds.alpha;
 
 
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: facebookAppId,
-    xfbml: true,
-    version: 'v2.8',
-  });
-  FB.AppEvents.logPageView();
-};
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: facebookAppId,
+      xfbml: true,
+      version: 'v2.8',
+    });
+    FB.AppEvents.logPageView();
+  };
 
 
-function createFacebookLiveVideo(callback) {
-  FB.getLoginStatus(function(response) {
-    if (response.status == 'connected') {
-      console.info({
-        Facebook: 'Already logged in.',
-      });
-      createFacebookLiveVideoAfterLogin(callback);
-    } else {
-      FB.login(function(response) {
-        if (!response.authResponse) {
-          console.warn({
-            Facebook: 'Login failed.',
-          });
-          return;
-        }
+  function createFacebookLiveVideo(callback) {
+    FB.getLoginStatus(function(response) {
+      if (response.status == 'connected') {
         console.info({
-          Facebook: 'Logged in, OK.',
-          grantedScopes: response.authResponse.grantedScopes,
+          Facebook: 'Already logged in.',
         });
         createFacebookLiveVideoAfterLogin(callback);
-      }, {
-        scope: 'publish_actions',
-        return_scopes: true,
-      });
-    }
-  });
-}
-
-
-function createFacebookLiveVideoAfterLogin(callback) {
-  FB.api('/me/live_videos', 'post', function(response) {
-    if (response.error) {
-      console.error({
-        Facebook: response.error.message,
-      });
-      return;
-    }
-    console.info({
-      Facebook: '/me/live_videos',
-      response: response,
+      } else {
+        FB.login(function(response) {
+          if (!response.authResponse) {
+            console.warn({
+              Facebook: 'Login failed.',
+            });
+            return;
+          }
+          console.info({
+            Facebook: 'Logged in, OK.',
+            grantedScopes: response.authResponse.grantedScopes,
+          });
+          createFacebookLiveVideoAfterLogin(callback);
+        }, {
+          scope: 'publish_actions',
+          return_scopes: true,
+        });
+      }
     });
-    callback(response.stream_url);
-  });
-}
+  }
 
-return exports;
+
+  function createFacebookLiveVideoAfterLogin(callback) {
+    FB.api('/me/live_videos', 'post', function(response) {
+      if (response.error) {
+        console.error({
+          Facebook: response.error.message,
+        });
+        return;
+      }
+      console.info({
+        Facebook: '/me/live_videos',
+        response: response,
+      });
+      callback(response.stream_url);
+    });
+  }
+
+  return exports;
 })();
