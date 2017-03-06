99e2d9ad73794cbf8088cd354bbdac31142c77e4
Namki Kim <dkd1111@ncsoft.com>
2017-03-06 11:52:10 +0900

    modified facebook.js path

--- tools/gjslint/gjslint_ncstreamer_ui.bat
+++ tools/gjslint/gjslint_ncstreamer_ui.bat
@@ -3,7 +3,7 @@
 set SrcRoot=../../html
 
 call gjslint.exe %SrcRoot%/app.js
-call gjslint.exe -nojsdoc %SrcRoot%/facebook.js
+call gjslint.exe -nojsdoc %SrcRoot%/lib/facebook.js
 call gjslint.exe -nojsdoc %SrcRoot%/main.js
 call gjslint.exe -nojsdoc %SrcRoot%/message.js
 call gjslint.exe -nojsdoc %SrcRoot%/localization.js
