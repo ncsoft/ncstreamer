7fc1aaaddce8fd8cdf580e0b87c807fc8485b5e3
Kyeongho Park <khpark@ncsoft.com>
2017-03-17 12:51:54 +0900

    LC-2019 #time 1h implement StreamingServiceProvider class.

---
+++ src/streaming_service/streaming_service_provider.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2017 NCSOFT Corporation
+ */
+
+
+#ifndef SRC_STREAMING_SERVICE_STREAMING_SERVICE_PROVIDER_H_
+#define SRC_STREAMING_SERVICE_STREAMING_SERVICE_PROVIDER_H_
+
+
+namespace ncstreamer {
+class StreamingServiceProvider {
+ public:
+  StreamingServiceProvider();
+  virtual ~StreamingServiceProvider() = 0;
+};
+}  // namespace ncstreamer
+
+
+#endif  // SRC_STREAMING_SERVICE_STREAMING_SERVICE_PROVIDER_H_
