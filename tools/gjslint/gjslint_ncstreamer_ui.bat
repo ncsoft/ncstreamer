9d5defdf3b1cc56fa53dce8398aa19d132494a71
Kyeongho Park <khpark@ncsoft.com>
2017-03-17 18:43:55 +0900

    refactoring: remove redundant files.

--- tools/gjslint/gjslint_ncstreamer_ui.bat
+++ tools/gjslint/gjslint_ncstreamer_ui.bat
@@ -4,6 +4,3 @@ set SrcRoot=../../html
 
 call gjslint.exe %SrcRoot%/app.js
 call gjslint.exe -nojsdoc %SrcRoot%/lib/facebook.js
-call gjslint.exe -nojsdoc %SrcRoot%/main.js
-call gjslint.exe -nojsdoc %SrcRoot%/message.js
-call gjslint.exe -nojsdoc %SrcRoot%/localization.js
