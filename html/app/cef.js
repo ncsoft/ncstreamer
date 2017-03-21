0226ac91d98ef3b686dec1b05a5b80b00d444bd6
Kyeongho Park <khpark@ncsoft.com>
2017-03-21 12:32:07 +0900

    minor indent

--- html/app/cef.js
+++ html/app/cef.js
@@ -15,14 +15,14 @@ const cef = (function() {
 
 
   // private functions
-function request(type, args) {
-  let argsArr = [];
-  for (const key in args) {
-    if (args.hasOwnProperty(key)) {
-      argsArr.push([key, encodeURIComponent(args[key])].join('='));
+  function request(type, args) {
+    let argsArr = [];
+    for (const key in args) {
+      if (args.hasOwnProperty(key)) {
+        argsArr.push([key, encodeURIComponent(args[key])].join('='));
+      }
     }
+    const uri = ['cef://', type, '?', argsArr.join('&')].join('');
+    location.href = uri;
   }
-  const uri = ['cef://', type, '?', argsArr.join('&')].join('');
-  location.href = uri;
-}
 })();
