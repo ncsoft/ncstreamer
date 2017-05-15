fb684f0432e00f2d299cb5feb3d9aa51fe9d9c28
Namki Kim <dkd1111@ncsoft.com>
2017-05-15 20:25:25 +0900

    LC-2424 #time 4h modify select control

--- ncstreamer_ui/static/app/custom_style.js
+++ ncstreamer_ui/static/app/custom_style.js
@@ -7,26 +7,15 @@ $(document).ready(function() {
   var event = new CustomEvent('customSelectChange');
 
   // custom select
-  $(document).on('click', '[data-ui="custom-select"] .opener', function(e) {
-    $(this).toggleClass('active');
-  });
-
-  $(document).on('click', '[data-ui="custom-select"] li', function(e) {
-    $(this).parent().prev().removeClass('active').text($(this).text());
-    $(this).parent().next().val($(this).attr('data-value'));
-    if (!($(this).hasClass('selected'))) {
-      $(this).addClass('selected').siblings().removeClass('selected');
-      document.querySelector(
-          '#' + $(this).closest('[data-ui="custom-select"]').attr('id')
-              ).dispatchEvent(event);
-    }
-  });
-
-  $(document).on('click', function(e) {
-    if (!($(e.target).is('[data-ui="custom-select"] .opener') ||
-        $(e.target).is('[data-ui="custom-select"] li'))) {
-      $('[data-ui="custom-select"] .opener').removeClass('active');
-    }
+  $(document).on('click', '.dropdown-menu li a', function(e) {
+    var selText = $(this).text();
+    $(this).parents('.dropdown').find('.dropdown-toggle').html(
+        selText + ' <span class="caret"></span>');
+    $(this).parents('.dropdown').find('.dropdown-toggle').val(
+        $(this).parent().attr('data-value'));
+    document.querySelector(
+        '#' + $(this).parents('.dropdown').attr('id')
+        ).dispatchEvent(event);
   });
 
   // tooltip
