ad1dd3af8a4fb2e4378e9288e83fe736a8077cbc
Kyeongho Park <khpark@ncsoft.com>
2017-05-18 16:34:02 +0900

    minor redundancy: call

--- ncstreamer_ui/build_tools/generate_static_ui_per_locale.bat
+++ ncstreamer_ui/build_tools/generate_static_ui_per_locale.bat
@@ -11,5 +11,5 @@ set Texts=%ProjectDir%..\ncstreamer_ui\localized_texts.json
 set TemplateDir=%ProjectDir%..\ncstreamer_ui\template
 set OutputDir=%ProjectDir%build\ncstreamer_ui\static_ui
 
-call "%Generator%" --texts="%Texts%" --input-dir="%TemplateDir%" --output-dir="%OutputDir%"
+"%Generator%" --texts="%Texts%" --input-dir="%TemplateDir%" --output-dir="%OutputDir%"
 @echo Generating static UI per locale ends: %ERRORLEVEL%
