5f152ebd389f486060754f2c9588b4cd9dee5da1
Kyeongho Park <khpark@ncsoft.com>
2017-05-18 16:29:53 +0900

    minor refactoring: trailing backslash

--- ncstreamer_cef/build_tools/import_ui.bat
+++ ncstreamer_cef/build_tools/import_ui.bat
@@ -4,13 +4,13 @@
 set ProjectDir=%~1
 set Configuration=%~2
 
-set FromDir=%ProjectDir%build\ncstreamer_ui\static_ui\
+set FromDir=%ProjectDir%build\ncstreamer_ui\static_ui
 if not exist "%FromDir%" (
   echo FromDir doesn't exist: %FromDir%
   exit /b 1
 )
 
-set ToDir=%ProjectDir%build\ncstreamer_cef\%Configuration%\bin\32bit\ui\
+set ToDir=%ProjectDir%build\ncstreamer_cef\%Configuration%\bin\32bit\ui
 
 robocopy %FromDir% %ToDir% /e
 @echo Importing UI ends.
