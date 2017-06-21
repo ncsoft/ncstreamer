982e5aa15a99a520881b237121ea315b4329edb1
Kyeongho Park <khpark@ncsoft.com>
2017-06-22 00:25:38 +0900

    call gjslint as strict mode.

--- vs14.0/build_tools/gjslint/gjslint_ncstreamer_ui.bat
+++ vs14.0/build_tools/gjslint/gjslint_ncstreamer_ui.bat
@@ -2,7 +2,7 @@
 
 set SrcRoot=../../../ncstreamer_ui
 
-call gjslint.exe --nojsdoc^
+call gjslint.exe --nojsdoc --strict^
  %SrcRoot%/template/app.js^
  %SrcRoot%/static/app/cef.js^
  %SrcRoot%/static/lib/ncsoft/ncsoft.js
