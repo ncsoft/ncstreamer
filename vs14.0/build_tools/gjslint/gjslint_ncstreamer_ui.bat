42f08ea4005d01ff13105e736a3a614185e5cb3b
Kyeongho Park <khpark@ncsoft.com>
2017-06-22 00:26:33 +0900

    minor log: gjslint

--- vs14.0/build_tools/gjslint/gjslint_ncstreamer_ui.bat
+++ vs14.0/build_tools/gjslint/gjslint_ncstreamer_ui.bat
@@ -2,7 +2,10 @@
 
 set SrcRoot=../../../ncstreamer_ui
 
+@echo gjslint begins.
 call gjslint.exe --nojsdoc --strict^
  %SrcRoot%/template/app.js^
  %SrcRoot%/static/app/cef.js^
  %SrcRoot%/static/lib/ncsoft/ncsoft.js
+
+@echo gjslint ends.
