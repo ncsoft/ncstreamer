6d8c2f148a9c108314e43e69b353f3d75a5be222
namk <dkd1111@ncsoft.com>
2017-08-10 16:30:45 +0900

    minor indent

--- vs14.0/build_tools/cpplint/cpplint_directory.bat
+++ vs14.0/build_tools/cpplint/cpplint_directory.bat
@@ -6,25 +6,25 @@ set CppLintDir=%1
 set TargetDir=%2
 
 set Index=0
-set FilesAll=
+set Files=
 for /r %TargetDir% %%f in (*.h *.cc) do (
   set /a Index+=1
-  set FilesAll=!FilesAll! %%f
+  set Files=!Files! %%f
   if !Index! geq 50 (  rem command-line string limitation
     pushd "%~dp0"
-    call python.exe "%CppLintDir%/cpplint.py" --output=vs7 !FilesAll!
+    call python.exe "%CppLintDir%/cpplint.py" --output=vs7 !Files!
     if %errorlevel% neq 0 (
       echo Error: cpplint
       exit /b %errorlevel%
     )
     popd
-	set FilesAll=
+	set Files=
 	set Index=0
   )
 )
 
 pushd "%~dp0"
-call python.exe "%CppLintDir%/cpplint.py" --output=vs7 %FilesAll%
+call python.exe "%CppLintDir%/cpplint.py" --output=vs7 %Files%
 if %errorlevel% neq 0 (
   echo Error: cpplint
   exit /b %errorlevel%
