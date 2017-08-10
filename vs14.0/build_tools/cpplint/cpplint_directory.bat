50e0368ec94b1bf15520d7795b6e719ebd9d8793
namk <dkd1111@ncsoft.com>
2017-08-10 16:30:02 +0900

    resolve command-line string limitation

--- vs14.0/build_tools/cpplint/cpplint_directory.bat
+++ vs14.0/build_tools/cpplint/cpplint_directory.bat
@@ -5,9 +5,22 @@ setlocal EnableDelayedExpansion
 set CppLintDir=%1
 set TargetDir=%2
 
+set Index=0
 set FilesAll=
 for /r %TargetDir% %%f in (*.h *.cc) do (
+  set /a Index+=1
   set FilesAll=!FilesAll! %%f
+  if !Index! geq 50 (  rem command-line string limitation
+    pushd "%~dp0"
+    call python.exe "%CppLintDir%/cpplint.py" --output=vs7 !FilesAll!
+    if %errorlevel% neq 0 (
+      echo Error: cpplint
+      exit /b %errorlevel%
+    )
+    popd
+	set FilesAll=
+	set Index=0
+  )
 )
 
 pushd "%~dp0"
