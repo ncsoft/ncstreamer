a2937befd356f09bbd7cf0f49c91974c57e2c705
namk <dkd1111@ncsoft.com>
2017-08-14 15:07:10 +0900

    bugfix: exit cpplint in error

--- vs14.0/build_tools/cpplint/cpplint_directory.bat
+++ vs14.0/build_tools/cpplint/cpplint_directory.bat
@@ -12,12 +12,20 @@ for /r %TargetDir% %%f in (*.h *.cc) do (
   set Files=!Files! %%f
   if !Index! geq 50 (  rem command-line string limitation
     call:executeLint "%CppLintDir%/cpplint.py" "!Files!"
+	if !errorlevel! neq 0 (
+      echo Error: cpplint
+      exit /b !errorlevel!
+    )
 	set Files=
 	set Index=0
   )
 )
 
 call:executeLint "%CppLintDir%/cpplint.py" "%Files%"
+if %errorlevel% neq 0 (
+  echo Error: cpplint
+  exit /b %errorlevel%
+)
 echo cpplint ends.
 exit /b %errorlevel%
 
@@ -25,9 +33,5 @@ exit /b %errorlevel%
 :executeLint
 pushd "%~dp0"
 call python.exe %1 --output=vs7 %~2
-if %errorlevel% neq 0 (
-  echo Error: cpplint
-  exit /b %errorlevel%
-)
 popd
 exit /b %errorlevel%
